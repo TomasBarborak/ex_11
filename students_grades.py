deeb49b64871ca3959d4aacdcdaf4016f5f1592b
Tomáš Barbořák <tom.barborak@gmail.com>
2026-04-22 10:43:00 +0200

    classy rozpracovano

---
+++ students_grades.py
@@ -0,0 +1,46 @@
+from itertools import count
+
+
+class StudentsGrades:
+    def __init__(self, scores):
+        self.scores = scores
+
+    def get_by_index(self, index):
+        return self.scores[index]
+
+    def count(self):
+        return len(self.scores)
+
+    def get_grade(self, index):
+        score = self.scores[index]
+        if score >= 90:
+            grade = "A"
+        elif score < 90 and score >= 80:
+            grade = "B"
+        elif score < 80 and score >= 70:
+            grade = "C"
+        elif score < 70 and score >= 60:
+            grade = "D"
+        elif score < 60 and score >= 50:
+            grade = "E"
+        else:
+            grade = "F"
+        return grade
+
+    def find(self, points):
+        points_index= []
+        for i in range(len(self.scores)):
+            if self.scores[i] == points:
+                points_index.append(i)
+        return points_index
+
+
+
+
+if __name__ == "__main__":
+    results = StudentsGrades([85, 42, 91, 67, 50, 73, 100, 38, 58])
+    print(results.count())  # 9
+    print(results.get_by_index(2))  # 91
+    print(results.scores)  # [85, 42, 91, 67, 50, 73, 100, 38, 58]
+    print(results.get_grade(index=1))
+    print(results.find(100))
