5dbec07dea142b944810e9d88bb3498768d2913b
Tomáš Barbořák <tom.barborak@gmail.com>
2026-04-22 09:04:11 +0200

    selection sort

--- sorting.py
+++ sorting.py
@@ -5,8 +5,26 @@ def random_numbers(count, low=0, high=100):
 
 
 
+def selection_sort(values):
+    for j in range(len(values)):
+        min_index = j
+        min_values = values[min_index]
+        for i in range(j +1,len(values)):
+            if values[i] < min_values:
+                min_index = i
+                min_values = values[i]
+
+        values[j], values[min_index] = values[min_index],values[j]
+    return values
+
+
+
+
+
+
 if __name__ == "__main__":
     values = random_numbers(10)  # 10 čísel v rozsahu 0–100
-    print(values)  # např. [42, 7, 91, 15, 63, 8, 57, 73, 2, 100]
-
+    print(values)  # např. [42, 7, 91, 15, 63, 8, 57, 73, 2, 100
     small = random_numbers(5, low=0, high=20)  # 5 čísel v rozsahu 0–20
+    selection = selection_sort(values)
+    print(selection)
